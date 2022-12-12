3d52e3415f2d5a2aaea6f13293a826f44e0be9cb
p16kyri <93210074+p16kyri@users.noreply.github.com>
2022-12-12 18:13:42 +0200

    Delete convertmd.sh

--- convertmd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for file in *md; do
-	echo "	Making ${file} to ${file::len-3}.pdf..."
-	pandoc -N --variable "geometry=margin=1.1in" --variable mainfont="OpenSans-Regular.ttf" --variable fontsize=13pt --variable variable version=2.0 ${file} --pdf-engine=xelatex --toc -o ${file::len-3}.pdf
-	echo "	Making ${file} to ${file::len-3}.docx..."
-	pandoc -s ${file} -o ${file::len-3}.docx
-done
